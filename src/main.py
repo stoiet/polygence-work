e68119dc3377e591c6f2dc5ffbef2818ef820ad3
Robert Beretka <to.stoiet@gmail.com>
2021-10-03 16:50:05 +0200

    Django application initialized

--- src/main.py
+++
@@ -1,5 +0,0 @@
-""" Hello World module """
-
-def hello_world():
-    """ Hello World methond """
-    return "Hello World!"
--- test/test_main.py
+++
@@ -1,5 +0,0 @@
-from src.main import hello_world
-
-def test_hello_world():
-    assert hello_world() == "Hello World!"
-    
