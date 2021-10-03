a007db5358443345e836f092d1eb17b4d9ae82e3
Robert Beretka <to.stoiet@gmail.com>
2021-10-03 17:36:49 +0200

    Spendings model introduced

--- spendings/models.py
+++ spendings/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Spendings(models.Model):
+    amount = models.DecimalField(max_digits=24, decimal_places=2)
+    currency = models.CharField(max_length=3)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
