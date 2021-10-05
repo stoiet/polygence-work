f05b1707e757770315603fe9f7bd3e2e742caf40
Robert Beretka <to.stoiet@gmail.com>
2021-10-05 15:47:45 +0200

    DELETE /api/spendings introduced

--- spendings/views.py
+++ spendings/views.py
@@ -7,3 +7,8 @@ from spendings.models import Spendings
 class SpendingsViewSet(viewsets.ModelViewSet):
    queryset = Spendings.objects.all()
    serializer_class = SpendingsSerializer
+
+   def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
